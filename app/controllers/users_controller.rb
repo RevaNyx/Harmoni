94da7a24a6c49285da14672cc1ea9e720237973a
Tamir Harris <tamirharris@Tamirs-MacBook-Air.local>
2024-12-11 12:28:56 -0600

    API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,30 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
+        @cronofy = cronofy_client
+        @authorization_url = @cronofy.user_auth_link('http://localhost:3000/oauth2/callback')
+    end
+
+    def connect
+        @cronofy = cronofy_client()
+        code = [params[:code]]
+        response = @cronofy.get_token_from_code(code, 'http://localhost:3000/oauth2/callback')
+
+        if User.where(account_id: response.account_id).exists?
+            user = User.find_by(account_id: response.account_id)
+            user.update(
+                access_token: response.access_token,
+                refresh_token: response.refresh_token
+            )
+            user.save
+        else
+            user = User.create(
+                account_id: response.account_id,
+                access_token: response.access_token,
+                refresh_token: response.refresh_token
+            )
+        end
+
+        redirect_to root_path
     end
 end
